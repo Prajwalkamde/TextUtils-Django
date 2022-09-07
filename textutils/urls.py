ac5992712f237a92cebe4ff743a05eb114e4431a
Prajwal Kamde <88785521+Prajwalkamde@users.noreply.github.com>
2022-09-07 15:43:34 +0530

    Update urls.py

--- textutils/urls.py
+++ textutils/urls.py
@@ -19,6 +19,7 @@ from . import views
 from django.conf import settings
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,8 +27,6 @@ urlpatterns = [
     path('about',views.about,name='about'),
     path('analyze',views.analyze,name='analyze'),
 
-#Added
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
    
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
